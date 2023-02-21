c2103bd61aaa8f8248f20a6635a84455bcc83069
zeidensad <100368432+zeidensad@users.noreply.github.com>
2023-02-21 14:07:59 +0100

    Rename Ip logger.py to port-scan.py

--- port-scan.py
+++ port-scan.py
@@ -4,7 +4,7 @@ from colorama import Fore, Style
 
 #je mes beaucoup d'ashta
 
-# Fonction de vérification de l'adresse IP
+# Fonction de vÃ©rification de l'adresse IP
 def is_valid_ip(address):
     try:
         ipaddress.ip_address(address)
@@ -14,39 +14,39 @@ def is_valid_ip(address):
 
 # Fonction de scan de port
 def port_scan(host, port):
-    # Créez un objet socket
+    # CrÃ©ez un objet socket
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(0.5)
     try:
-        # Tentez de vous connecter à l'adresse et au port spécifiés
+        # Tentez de vous connecter Ã  l'adresse et au port spÃ©cifiÃ©s
         s.connect((host, port))
         print(f"{Fore.GREEN}Le port {port} est ouvert{Style.RESET_ALL}")
     except:
-        # Si la connexion échoue, le port est probablement fermé
-        print(f"{Fore.RED}Le port {port} est fermé{Style.RESET_ALL}")
+        # Si la connexion Ã©choue, le port est probablement fermÃ©
+        print(f"{Fore.RED}Le port {port} est fermÃ©{Style.RESET_ALL}")
     finally:
         # Fermer la connexion
         s.close()
 
-# Afficher une bannière "Port Scanner"
+# Afficher une banniÃ¨re "Port Scanner"
 print(f"{Fore.CYAN}\n{'-'*50}\nPort Scanner\n{'-'*50}{Style.RESET_ALL}\n")
 
-# Obtenez le nom d'hôte ou l'adresse IP du site web à scanner
-target_host = input("Entrez l'adresse IP du site web à scanner: ")
+# Obtenez le nom d'hÃ´te ou l'adresse IP du site web Ã  scanner
+target_host = input("Entrez l'adresse IP du site web Ã  scanner: ")
 
-# Vérifier si l'adresse IP est valide
+# VÃ©rifier si l'adresse IP est valide
 if not is_valid_ip(target_host):
     print(f"{Fore.RED}Adresse IP invalide{Style.RESET_ALL}")
     exit()
 
-# Définir les ports à scanner (par défaut, nous scannerons les ports 1 à 1024)
+# DÃ©finir les ports Ã  scanner (par dÃ©faut, nous scannerons les ports 1 Ã  1024)
 target_ports = range(1, 1025)
 
-# Demander à l'utilisateur s'il souhaite exécuter le scanner de port ou accéder aux informations
-option = input("Entrez '1' pour scanner les ports, ou '2' pour accéder aux informations: ")
+# Demander Ã  l'utilisateur s'il souhaite exÃ©cuter le scanner de port ou accÃ©der aux informations
+option = input("Entrez '1' pour scanner les ports, ou '2' pour accÃ©der aux informations: ")
 
 if option == "1":
-    # Boucle à travers les ports cibles et effectuez le scan de port
+    # Boucle Ã  travers les ports cibles et effectuez le scan de port
     for port in target_ports:
         port_scan(target_host, port)
 elif option == "2":
